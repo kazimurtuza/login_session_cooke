5b1ba2b65362ff5b3a01e0b8cf4595161b57572c
kazimurtuza <kazimurtuza11@gmail.com>
2019-10-21 20:21:15 +0600

    exception control

--- index.php
+++ index.php
@@ -30,5 +30,25 @@ echo filter_var($info,FILTER_CALLBACK,array("options"=>"convert"));
 
 
 
+// ///////////////exception controle ////////
+
+$a=9;
+try{
+    if($a<5)
+    {
+        echo "value in under 10"."<br/>";
+    }
+    else{
+        throw new Exception("value is not under 10");
+    }
+}
+catch(Exception $e)
+{
+    echo "<br/>". $e->getMessage()."<br/>";
+}
+
+finally{
+    echo "finally block"."<br/>";
+}
 
 ?>
